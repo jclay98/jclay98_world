082fa75061f810419767328f6e79994152664bf7
Kasdan Bakos <kbakos@andrew.cmu.edu>
2019-02-19 22:24:19 -0500

    Just to add all in

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 
     validates_presence_of :title, :content
 
-    scope :active,           -> { where('active = ?', true) }
+    scope :active, -> { where('active = ?', true) }
     scope :alphabetical, -> { order('title') }
 end
